6ac1bf4696a9c3a45399138bb23f66ca7775485d
Matt Zagrabelny <mzagrabe@d.umn.edu>
2014-07-14 10:06:20 -0500

    initial commit

---
+++ lib/RT/Extension/SetOwnerRequestorToCurrentUserForTicketCreate.pm
@@ -0,0 +1,119 @@
+package RT::Extension::SetOwnerRequestorToCurrentUserForTicketCreate;
+
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+RT::Extension::SetOwnerRequestorToCurrentUserForTicketCreate - The great new RT::Extension::SetOwnerRequestorToCurrentUserForTicketCreate!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use RT::Extension::SetOwnerRequestorToCurrentUserForTicketCreate;
+
+    my $foo = RT::Extension::SetOwnerRequestorToCurrentUserForTicketCreate->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Matt Zagrabelny, C<< <mzagrabe at d.umn.edu> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-rt-extension-setownerrequestortocurrentuserforticketcreate at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=RT-Extension-SetOwnerRequestorToCurrentUserForTicketCreate>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc RT::Extension::SetOwnerRequestorToCurrentUserForTicketCreate
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=RT-Extension-SetOwnerRequestorToCurrentUserForTicketCreate>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/RT-Extension-SetOwnerRequestorToCurrentUserForTicketCreate>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/RT-Extension-SetOwnerRequestorToCurrentUserForTicketCreate>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/RT-Extension-SetOwnerRequestorToCurrentUserForTicketCreate/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Matt Zagrabelny.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 dated June, 1991 or at your option
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+A copy of the GNU General Public License is available in the source tree;
+if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+
+=cut
+
+1; # End of RT::Extension::SetOwnerRequestorToCurrentUserForTicketCreate
